e80abda3742cf75e6419ec02e9b34a040466fe48
whjkh123 <whjkh123@naver.com>
2020-11-17 10:30:01 +0900

    2020. 11. 17 final

--- src/com/javaex/ex04/Ex07.java
+++ src/com/javaex/ex04/Ex07.java
@@ -3,14 +3,13 @@ package com.javaex.ex04;
 public class Ex07 {
 
 	public static void main(String[] args) {
-		
-		String star = "******";
-		
-		for(int i = 1; i <= 6; i++) {
-			
-			System.out.println(star);
-		}
-		
+				
+		for(int y = 0; y < 6; y++) {
+			for(int x = 0; x < 6; x++) {
+			System.out.print("*");
+			}System.out.println();
+		}	
+				
 	}
 	
 }
--- src/com/javaex/ex04/Ex08.java
+++ src/com/javaex/ex04/Ex08.java
@@ -3,14 +3,11 @@ package com.javaex.ex04;
 public class Ex08 {
 
 	public static void main(String[] args) {
-				
-		int star = 1;
 		
 		for(int line = 1; line <= 6; line++) {
-			for(int x = 0; x < star; x++) {
-				System.out.print('*');
-			}						
-			star += 1;
+			for(int x = 1; x <= line; x++) {
+				System.out.print("*");
+			}		
 			System.out.println();
 		}
 	}
